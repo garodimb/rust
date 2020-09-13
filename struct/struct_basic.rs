b74b116fa556da85d36ae6331402c4f6db3164bb
Mahendra Garodi <garodimb@gmail.com>
2020-09-13 06:24:41 +0000

    Build struct using function and another instance

--- struct/struct_basic.rs
+++ struct/struct_basic.rs
@@ -1,19 +1,53 @@
+struct User
+{
+  username: String,
+  email: String,
+  sign_in_count: u64,
+  active: bool
+}
+
 fn main()
 {
-  struct User
-  {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-  };
-
-  let user1 = User {
-    email: String::from("abc@xyz.com"),
-    username: String::from("abc"),
-    sign_in_count: 1,
+  let user1 = User{
+    username: String::from("Gopal"),
+    email: String::from("gopal@xyz.com"),
     active: true,
+    sign_in_count: 1
   };
 
-  println!("Email Address: {}", user1.email);
+  // Constructing struct using partial members of another instance
+  let user2 = User{
+    username: String::from("Amol"),
+    email: String::from("amol@gmail.com"),
+    ..user1
+  };
+
+  display_user(&user1); // Copy Semantics
+  println!("------------------------");
+  display_user(&user2);
+
+  // construct struct using function paramater having same name as struct
+  let user3 = build_user(String::from("Ajay"), String::from("ajay@gmail.com"));
+  println!("------------------------");
+  display_user(&user3);
+}
+
+
+fn display_user(user: &User)
+{
+  println!("Username: {}", user.username);
+  println!("Email: {}", user.email);
+  println!("Sign-In Count: {}", user.sign_in_count);
+  println!("Active: {}", user.active);
+}
+
+// construct struct using function paramater having same name as struct
+fn build_user(username: String, email: String) -> User
+{
+  User{
+    username,
+    email,
+    sign_in_count: 1,
+    active: false
+  }
 }
