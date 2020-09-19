87aa23f13d23c4e34428f0d8a248073c9fc09f3f
Mahendra Garodi <garodimb@gmail.com>
2020-09-19 05:06:02 +0000

    Generate secret number using rand

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,29 @@
+/*
+ * Guessing Game
+ */
+
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main()
 {
+  let secret_number = rand::thread_rng().gen_range(1, 11);
+
   println!("Guessing Game!");
   let mut guess = String::new();
+  
   println!("Enter your guess:");
+  
   std::io::stdin().read_line(&mut guess).expect("Unable to read input");
   println!("You guessed {}", guess);
+
+  println!("Secret Number: {}", secret_number);
+
+  let guess : u32 = guess.trim().parse().expect("Unable to parse number!");
+  match guess.cmp(&secret_number)
+  {
+    Ordering::Less => println!("Too small!"),
+    Ordering::Greater => println!("Too large!"),
+    Ordering::Equal => println!("You won!"),
+  }
 }
